3f8d993d1df20cebfaa1ef3d4f8df5da35a6123a
THOMAS ENGLAND <tom@england.dev>
2025-06-04 19:51:01 -1000

    yellow triangle

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -3,6 +3,6 @@ out vec4 FragColor;
 
 void main()
 {
-    // Set the fragment color to white
-    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    // Set the fragment color to yellow
+    FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);
 }
